161297e09dbac510cd094fa4025349af0a3532a2
haesoox3 <monicchoe@gmail.com>
2017-08-13 22:17:22 -0400

    Post route/formatting fields helper done. Working on sequelize types

---
+++ server/api/tables.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const Sequelize = require('sequelize');
+const db = require('./db');
+module.exports = router;
+
+// req.body : array of objects
+// each object represents one table
+// ex/ [{tableName : 'table1', 
+//          fields : {name : {type: String, validations: ...}, 
+//                    quantity : {type : integer, validations: ...}}}, 
+//      {tableName : 'table2', 
+//          fields : {name : {type: String, validations: ...}, ...}]
+router.post('/', (req, res, next) => {
+    let tables = req.body; 
+    for (var table of tables){
+        let tableName = table.tableName;
+        let fields = table.fields; 
+        const createdTable = db.define(tableName, fields);
+        db.sync();
+    }
+});
+
+function getSequelizeType(type){
+    let d = {};
+}
+
+function formatFields(fields){
+    let keys = Object.keys(fields);
+    // ['name', 'quantity']
+    for (var field of keys){
+        let attribute = fields[field]; 
+        let seqType = attribute['type']
+        fields[field] = Object.assign({}, attribute, {type: getSequelizeType(seqType)})
+    }
+    return fields
+}
