4a3b3de7e4f8fa1cea7efd459c95ad47eb93f712
vinayarv <vinayarv@users.noreply.github.com>
2017-08-14 23:13:27 -0400

    added code to create new db for user

--- server/api/test.js
+++ server/api/test.js
@@ -1,6 +1,13 @@
 var client = require('../db').client;
 var router = require('express').Router();
+const pgtools = require('pgtools');
 
+const config = {
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  host: process.env.RDS_HOSTNAME
+}
 
 router.get('/', (req, res, next) => {
   console.log("In test");
@@ -10,6 +17,17 @@ router.get('/', (req, res, next) => {
     res.send(result);
   })
   .catch(next);
+});
+
+router.get('/create', (req, res, next) => {
+  console.log("creating database for a user");
+  pgtools.createdb(config, 'test-db-aws')
+  .then( (result) => {
+    console.log("Created db: ", result)
+    res.send(result)
+  })
+  .catch(next);
+
 })
 
 module.exports = router;
