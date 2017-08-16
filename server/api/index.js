7e3a3c228a5b82913fb08f0ece9986e33b32c5cf
haesoox3 <monicchoe@gmail.com>
2017-08-16 11:04:52 -0400

    Set up metatables file

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/metatable', require('./metatable'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/metatable.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+POST to metatable --> databse ID, name
+
+DELETE from metatable --> tableID
+
+GET all tables associated to a database metatable --> database ID
