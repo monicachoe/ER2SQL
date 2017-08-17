f7ca1dcac159555034a8402fe1d863636f6cbc4e
brendasukh <sukhnyamdorj@Gantsetsegs-MacBook-Air.local>
2017-08-17 18:12:12 -0400

    getting column data back from table using sql query

--- server/api/metatable.js
+++ server/api/metatable.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {Table} = require('../db/models')
+const client = require('../db/client');
 module.exports = router
 
 router.get('/:tableId', (req, res, next) => {
@@ -8,6 +9,13 @@ router.get('/:tableId', (req, res, next) => {
     .then((table) => res.json(table))
     .catch(next)
 })
+router.get('/:tableId/columns', (req,res,next) => {
+  var tableId = req.params.tableId
+  client.query(`SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '1s'`)
+    .then((columns) => 
+      res.json(columns.rows)
+    )
+  })
 
 router.post('/', (req, res, next) => {
   var name  = req.body.tableName;
