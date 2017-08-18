da718a01ff2816330294b45c7ffc15d647209b8f
Kelaiya <kelaiyarao1@gmail.com>
2017-08-18 12:03:07 -0400

    merge branch

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/fields', require('./fields'))
 router.use('/tables', require('./tables'))
 router.use('/metatable', require('./metatable'))
 router.use('/metadatabase', require('./metadatabase'))
