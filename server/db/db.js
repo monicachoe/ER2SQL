05dcfc2f4e98d660aa906c093e6c9c2ad0f59b08
haesoox3 <monicchoe@gmail.com>
2017-08-18 14:18:19 -0400

    pulling combined

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,20 @@
-const Sequelize = require('sequelize')
+// const Sequelize = require('sequelize')
 
-const db = new Sequelize( process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD,
-  {
-    host: process.env.RDS_HOSTNAME,
-    dialect: 'postgres',
-    port: process.env.RDS_PORT,
+// const db = new Sequelize( process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD,
+//   {
+//     host: process.env.RDS_HOSTNAME,
+//     dialect: 'postgres',
+//     port: process.env.RDS_PORT,
+//     logging: false
+//   }
+// )
+// module.exports = db
+
+const Sequelize = require('sequelize')
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/er2sql', {
     logging: false
   }
 )
-module.exports = db
+
+module.exports = db;
