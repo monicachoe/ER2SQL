52e2df66721c35b07f00b9d0cd5adb8c78092742
haesoox3 <monicchoe@gmail.com>
2017-08-17 22:19:13 -0400

    fixed loabdb

--- client/components/index.js
+++ client/components/index.js
@@ -3,7 +3,7 @@ export {default as UserHome} from './user-home';
 export {Login, Signup} from './auth-form';
 export {default as CreateTable} from './CreateTable';
 export {default as CreateDB} from './createdb'
-export {default as LoadDb} from './loaddb'
+export {default as LoadDB} from './loaddb'
 export {default as AddAssociation} from './AddAssociation';
 export {default as AddField} from './AddField';
 export {default as RemoveTable} from './RemoveTable';
